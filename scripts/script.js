0944622f9925fa891334ee8a7e8da8c5359ef636
Pagan <paganseacrest@gmail.com>
2020-12-30 04:14:43 +0300

    exception scripts

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,61 @@
-class MarkupTask {
-  constructor ()
-
-  books = [
-    { 
-      author: "Скотт Бэккер",
-      name: "Тьма, что приходит прежде",
-      price: 70 
-    }, 
+books = [
     {
-     author: "Скотт Бэккер",
-     name: "Воин-пророк",
-    }, 
-    { 
-      name: "Тысячекратная мысль",
-      price: 70
-    }, 
-    { 
-      author: "Скотт Бэккер",
-      name: "Нечестивый Консульт",
-      price: 70
-    }, 
-    {
-     author: "Дарья Донцова",
-     name: "Детектив на диете",
-     price: 40
+        author: "Скотт Бэккер",
+        name: "Тьма, что приходит прежде",
+        price: 70,
     },
     {
-      author: "Дарья Донцова",
-      name: "Дед Снегур и Морозочка",
+        author: "Скотт Бэккер",
+        name: "Воин-пророк",
+    },
+    {
+        name: "Тысячекратная мысль",
+        price: 70,
+    },
+    {
+        author: "Скотт Бэккер",
+        name: "Нечестивый Консульт",
+        price: 70,
+    },
+    {
+        author: "Дарья Донцова",
+        name: "Детектив на диете",
+        price: 40,
+    },
+    {
+        author: "Дарья Донцова",
+        name: "Дед Снегур и Морозочка",
     }
   ];
+
+function createMarkup(root) {
+    root = document.getElementById(root);
+
+    return (element, id) => {
+        element = document.createElement(element)
+        element.setAttribute("id", id);
+        root.append(element);
+    }
 }
 
-// ------------------------------------------------------
-  
-function createMarkup() {
-  const root = document.getElementById("root");
-  
-  return function(element) {
-    root.append(element);
-  }
-}
-// -------------------------------------------------------
-const append = createMarkup();
+function compose(arr) {
+    createMarkup("root")("ul", "ul");
 
-function fillElements () {
-  try{
-    books.map(element => element.author, element.name, element.price);
-    // append(document.createElement("ul")
-  } catch(err) {
-    console.log();(err.name = "Property Existence Error");
-  }
+    arr.forEach((element, index) => {
+        let id = "li" + index;
 
+        try {
+            if (Object.keys(element).length !== 3) {
+                throw new Error("More properties expected");
+            } else {
+                createMarkup("ul")("li", id);
+                document.getElementById(id).textContent = `Автор: ${element.author};
+                Название: ${element.name}; Цена: ${element.price}`;
+            }
+        } catch (err) {
+            console.log(`${err} in ${index + 1} element`);
+        }
+    });
 }
 
-fillElements();
-
-
-let ul = append(document.createElement("ul"));
-ul = append(document.createElement("li").textContent = "ky");
-
-ul();
+compose(books);
